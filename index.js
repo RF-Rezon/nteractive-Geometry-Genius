f5860dc9f47165cd64f961cfd6bdaf91c38c50ea
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-02-18 03:13:11 +0600

    Ui complete

---
+++ index.js
@@ -0,0 +1,7 @@
+let blogPage = function(){
+ document.open("./sceond_page.html");
+}
+
+
+let sideTextArea = document.getElementById("sideTextArea");
+sideTextArea.innerText = "tttttttccccccccccccccccccccccccccccccccccc";
